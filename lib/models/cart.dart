3978a00d645aaebf7b89cb510374d29cf2261b52
“Seleemmiah” <seleemmiah1@gmail.com>
2025-01-25 01:42:31 +0100

    update

---
+++ lib/models/cart.dart
@@ -0,0 +1,45 @@
+import 'shoe.dart';
+
+class Cart {
+  // list of shoes for sale
+  final List<Shoe> shoes = [
+    Shoe(
+      name: 'Nike Air Max 90',
+      price: '150',
+      description: 'Comfortable and stylish',
+      imagePath: 'lib/images/jordan1.JPG',
+    ),
+
+    // Add more Shoe objects
+    Shoe(
+      name: 'Nike Air Max Dn',
+      price: '300',
+      description: 'Iconic style, unparalleled comfort',
+      imagePath: 'lib/images/jordan.JPG',
+    ),
+
+    Shoe(
+      name: 'Nike Free Run 5.0',
+      price: '225',
+      description: 'Move naturally, feel unstoppable.',
+      imagePath: 'lib/images/jordan1.JPG',
+    ),
+
+    Shoe(
+      name: 'Nike Court Vision Low',
+      price: '159',
+      description: 'Walk on air, redefine your stride',
+      imagePath: 'lib/images/jordan.JPG',
+    ),
+  ];
+
+  //list of items in user cart
+
+  //getting the list of shoes for sale
+
+  // get cart
+
+  //add items to cart
+
+  // remove items from cart
+}
--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -20,28 +20,28 @@ class _ShopPageState extends State<ShopPage> {
       name: 'Nike Air Max 90',
       price: '150',
       description: 'Comfortable and stylish',
-      imagePath: 'lib/images/jordan.JPG',
+      imagePath: 'lib/images/jordan1.JPG',
     ),
 
     // Add more Shoe objects
     Shoe(
-      name: 'Nike Air Max 90',
-      price: '150',
-      description: 'Comfortable and stylish',
+      name: 'Nike Air Max Dn',
+      price: '300',
+      description: 'Iconic style, unparalleled comfort',
       imagePath: 'lib/images/jordan.JPG',
     ),
 
     Shoe(
-      name: 'Nike Air Max 90',
-      price: '150',
-      description: 'Comfortable and stylish',
-      imagePath: 'lib/images/jordan.JPG',
+      name: 'Nike Free Run 5.0',
+      price: '225',
+      description: 'Move naturally, feel unstoppable.',
+      imagePath: 'lib/images/jordan1.JPG',
     ),
 
     Shoe(
-      name: 'Nike Air Max 90',
-      price: '150',
-      description: 'Comfortable and stylish',
+      name: 'Nike Court Vision Low',
+      price: '159',
+      description: 'Walk on air, redefine your stride',
       imagePath: 'lib/images/jordan.JPG',
     ),
   ];
